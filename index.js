0836d599e0ecb88b1e095db902cfde836b9ed8a5
Psychotic_Force <devastation1209@gmail.com>
2023-04-13 01:07:17 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessageReactions,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.Guilds
+    ]
+});
+const { exec } = require("child_process");
+require("dotenv").config();
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+
+
+// exec("ls -la", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
+
+//screen -S minecraft -X stuff "say hi\n"
+
+
+client.login(process.env.TOKEN);
